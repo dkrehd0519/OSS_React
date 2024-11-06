96f32e1c12dfdeb24e49204e386e89189e90a71c
JongHyun <jonghyun@JongHyuns-MacBook-Pro.local>
2024-11-06 23:10:00 +0900

    change css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Component1 from "./component1";
+import Hello from "./component/Hello";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>react oss class 20241028</p>
+        <Hello />
         <Component1 />
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
